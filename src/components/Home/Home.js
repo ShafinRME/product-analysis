d5b734ea72cd070d01e7acd5c871ee00566f7e82
ShafinRME <shafin.rmedu4@gmail.com>
2022-04-04 01:11:01 +0600

    Alhamdulillah.Home page is finished.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import "./Home.css"
+import pic from "../../food.jfif"
 
 const Home = () => {
     return (
         <div>
-
+            <h2 className='website-name'>Food Mania</h2>
+            <div className='home-doc'>
+                <div>
+                    <h2 className='headline1'>Choose Your Best Food</h2>
+                    <h2 className='headline2'>Keep Your Health Good</h2>
+                    <p className='texts'>Hi,Welcome to Our Food Mania Official Website.Here you can explore many types of Food around the World.Every Food Item is 100% Good for Human Body.You can buy them from our website.Just click the Below Link</p>
+                    <button className='button-header'>Live Demo</button>
+                </div>
+                <div>
+                    <img className='image' src={pic} alt="Img is not found" />
+                </div>
+            </div>
+            <div>
+                <h1>Customer Reviews(3)</h1>
+                <button className='review-btn'>See All Reviews</button>
+            </div>
         </div>
     );
 };
