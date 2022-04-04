41747cd6d9924653913bcd37f2bf4cb2c02c8ba9
ShafinRME <shafin.rmedu4@gmail.com>
2022-04-05 01:10:36 +0600

    Alhamdulillah.Media Query finished.

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -8,25 +8,29 @@ const Reviews = () => {
     let [reviews, setReviews] = useLoadReviews();
 
     return (
-        <div className='reviews-all'>
-            {
-                reviews.map(rv =>
-                    <div className='review-head'>
-                        <div className='pic-name'>
-                            <img src={rv.picture} alt="pic is not found" />
-                            <h4>{rv.name}</h4>
-                        </div>
-                        <div>
-                            <p> <span className='comments'>Comments:</span>  <br />{rv.reviews}</p>
-                            <p className='ratings-head' ><span className='ratings'>Ratings: </span> <span className='ratings-star'>{rv.ratings} <FontAwesomeIcon icon={faStar} /></span>  </p>
+
+        <div >
+            <h1 className='all-reviews'>All Reviews</h1>
+            <div className='reviews-all'>
+
+                {
+                    reviews.map(rv =>
+                        <div className='review-head'>
+                            <div className='pic-name'>
+                                <img src={rv.picture} alt="pic is not found" />
+                                <h4>{rv.name}</h4>
+                            </div>
+                            <div>
+                                <p> <span className='comments'>Comments:</span>  <br />{rv.reviews}</p>
+                                <p className='ratings-head' ><span className='ratings'>Ratings: </span> <span className='ratings-star'>{rv.ratings} <FontAwesomeIcon icon={faStar} /></span>  </p>
+
+                            </div>
 
                         </div>
 
-                    </div>
-
-                )
-            }
-
+                    )
+                }
+            </div>
 
         </div>
     );
