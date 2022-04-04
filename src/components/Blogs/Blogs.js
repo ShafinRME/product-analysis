96f363e1e44e468520813f364ddc5cc76c15221e
ShafinRME <shafin.rmedu4@gmail.com>
2022-04-04 17:57:01 +0600

    Alhamdulillah.Reviews data is creating properly.

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -4,7 +4,35 @@ import "./Blog.css"
 const Blogs = () => {
     return (
         <div>
+            <h1>What is Context API?</h1>
+            <div className='first-blog'>
+                <p>
+                    The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on.The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on.
+                    React.createContext() is all you need. It returns a consumer and a provider. Provider is a component that as it's names suggests provides the state to its children. It will hold the "store" and be the parent of all the components that might need that store. Consumer as it so happens is a component that consumes and uses the state.
+                </p>
+            </div>
+            <h1>What is Semantic Tag?</h1>
+            <div className='second-blog'>
+                <p>
+                    Semantic HTML tags provide information about the contents of those tags that goes beyond just how they look on a page. Text that is enclosed in the tag is immediately recognized by the browser as some type of coding language.
+                    There are many types of Symantic Tag.Such as
+                    <div className='list-tag'>
+                        <ul >
+                            <li>header </li>
+                            <li>aside</li>
+                            <li>details</li>
+                            <li>article</li>
+                            <li>footer</li>
+                        </ul>
+                    </div>
+                    There are many advantages of using Semantic Tag in HTML.
 
+                    <h6>The semantic HTML tags help the search engines and other user devices to determine the importance and context of web pages.</h6>
+                    <h6>The pages made with semantic elements are much easier to read.</h6>
+                    <h6>It has greater accessibility. It offers a better user experience.</h6>
+
+                </p>
+            </div>
         </div>
     );
 };
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import "./NotFound.css"
+import image from "../../404.jpg"
 
 const NotFound = () => {
     return (
         <div>
-            <h1>404</h1>
+            <h1 className='dude'>...Hey dude...</h1>
+            <h1 className='sorry'>............Sorrryyyy.............</h1>
+            <h4 className='nothing'>Here is nothing for you without this photo.</h4>
+            <div>
+                <img className='pic' src={image} alt="Pictures is not found" />
+            </div>
         </div>
     );
 };
